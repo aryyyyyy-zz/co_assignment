df41313e8c66946bcc534b11f59242c022a657d7
arya <arya20498@iiitd.ac.in>
2021-08-12 14:02:10 +0530

    hlt command added

--- Simple-Assembler/code.py
+++ Simple-Assembler/code.py
@@ -16,11 +16,12 @@ def getRegister(token, flag) :
 
 lines = []
 var_list = []
+flag = False
 a_commands = {'add': "00000",'sub': "00001", 'mul': "00110", 'xor': "01010", 'or': "01011", 'and': "01100"}
 d_commands = {'ld': "00100", 'st': "00101"}
+f_commands = {"hlt": "10011"}
 
-while True :
-	line = input()
+for line in stdin:
 	if line == '': # If empty string is read then stop the loop
 		break
 	lines.append(line)
@@ -36,19 +37,41 @@ for line in lines:
 
 for line in lines[len(var_list) : ]:
 
+	if lines.index(line) == len(lines) - 1:
+
+		if len(line) != 3 :						#3-lettered command "hlt"
+			print("ERROR: Invalid syntax")
+			quit()
+
+		if line == "hlt" :
+			ans +=f_commands[line] + "0"*11
+
+		else :
+			#last instrcution is not hlt
+			flag = True
+
 	line = list(line.split(" "))
 
 	if line[0] in a_commands :
 
-		ans += a_commands[line[0]] + "00" #unused bits added
+		if len(line) != 4 :
+			print("ERROR: Invalid syntax")
+			quit()
 
-		if (getRegister(line[1], False) == '999' or getRegister(line[1], False) == '999' or getRegister(line[1], False) == '999') :
-			print("Invalid register name")
+		ans += a_commands[line[0]] + "00" #unused bits added
+		
+		if (getRegister(line[1], False) == '999' or getRegister(line[2], False) == '999' or getRegister(line[3], False) == '999') :
+			print("ERROR: Invalid register name")
 			quit()
 		x = getRegister(line[1], False) + getRegister(line[2], False) + getRegister(line[3], False)
 		ans += x
 
 	if line[0] in d_commands :
+
+		if len(line) != 3 :
+			print("ERROR: Invalid syntax")
+			quit()
+
 		ans += d_commands[line[0]]
 		x = getRegister(line[1], False) 
 		if x!="999" :
@@ -58,14 +81,23 @@ for line in lines[len(var_list) : ]:
 				ans += str(f'{n:08b}')
 				
 			else:
-				print("Use of undefined variable")
+				print("ERROR: Use of undefined variable")
 				quit()
 
 		else :
-			print("Invalid register name")
+			print("ERROR: Invalid register name")
 			quit()
+
+	if line[0] in f_commands and lines.index(line[0]) != len(lines) - 1 :
+
+		print("ERROR: hlt not being used as the last instruction")
+		quit()
 
 	print(ans)
 	ans = ""
+
+	if flag :
+		print("ERROR: Missing hlt instruction")
+		quit()
 
 
