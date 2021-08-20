e23a6859abb82fd5c04c751aa6295c9e1e73535c
MaddyG-05 <mudit20315@iiitd.ac.in>
2021-08-20 13:10:24 +0530

    tried a few things

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -39,12 +39,12 @@ class Simulator:
 		self.isHalted = False
 		self.registers = Registers()
 
-	def run(self):
-		input_lines = []
-		for line in stdin:
-			if line =='':
-				break
-			input_lines.append(line)
+	def run(self, input_lines):
+
+		# for line in stdin:
+		# 	if line =='':
+		# 		break
+		# 	input_lines.append(line)
 
 		for i in  range(len(input_lines)):
 			self.memory.write(i, input_lines[i])
@@ -232,6 +232,19 @@ class Simulator:
 		for i in range(self.memory.getSize()):
 			print(self.memory.read(i))
 
-if __name__ == "__main__" :
-	sim = Simulator()
-	sim.run()
+# def main() :
+# 	sim = Simulator()
+#     print('hello')
+# 	sim.run()
+
+# main()
+
+def main():
+    input_lines = os.read(0, 10**6).strip().splitlines()
+    for x in range(len(input_lines)):
+        input_lines[x] = input_lines[x].decode('utf-8')
+    
+    sim = Simulator()
+    sim.run(input_lines)
+
+main()
